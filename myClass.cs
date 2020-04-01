6556102e3d745c956372b25027b8990d0777bf33
Madeleine Glindorf <s181528@student.dtu.dk>
2020-04-01 10:49:29 +0200

    tilføjet navn, adresse og postnr

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,13 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        public string navn { get; set; }
+
+        public string address { get; set; }
+
+        public int postnr { get; set; }
+
+
+
     }
 }
